30777a5f6e689a68c74f99bcfcdcbce6c35763cd
Lucas Reis <108764828+LucasRe1s@users.noreply.github.com>
2023-10-06 18:13:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# desafios-diarios-codando-na-gringa
+ Desafios lançados e elaborados pelo @codandonagringa diariamente com o intuito de ajudar iniciantes e criar consistência!
---
+++ day-01.js
@@ -0,0 +1,16 @@
+/* 
+# Codando na gringa - desafios
+
+Bom diaaaaaaa!!! Nosso desafio de hoje é um desafio super simples 😊
+Dada a String: "Olá, tudo bem?" imprima na tela a 
+quantidade de caracteres que essa String possui
+
+Material de apoio:
+
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/length (exemplo em javascript)
+
+https://pt.wikipedia.org/wiki/Cadeia_de_caracteres
+*/
+
+let saudacao = "Olá, tudo bem?";
+console.log(saudacao.length);
