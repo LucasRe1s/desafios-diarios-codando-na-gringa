16b089787e16b1d6ed381777bcc7eae10c3473fe
Lucas Reis <108764828+LucasRe1s@users.noreply.github.com>
2023-10-08 15:32:44 -0300

    feat: challenge array reverse.

---
+++ day-03.js
@@ -0,0 +1,26 @@
+/*
+Bom diaaaa!!! Que seja um domingo incrível pra todos vocês ✨
+
+Vamos falar sobre Arrays? Um array é um conjunto de dados, hoje, vamos falar sobre um conjunto de números inteiros 
+
+Desafio de hoje:
+
+Dado o array de números inteiros [2, 3, 1, 5]
+Inverta o array e imprima-os invertidos [5, 1, 3, 2] - use um método já existente, 
+ver no material de apoio
+
+Modo complexo (para não iniciantes): Dado qualquer array de inteiros, use algum algoritmo de 
+ordenação e imprima o array em ordem crescente
+
+Material de Apoio
+- O que é um array (vetor)? - bit.ly/array-definicao
+- Inverter array usando um método em javascript: mzl.la/3RQHaNo
+
+Modo complexo:
+- bit.ly/algoritmo-ordenar-array
+*/
+
+let arr = [2, 3, 1, 5];
+arr.reverse();
+
+console.log(arr);
